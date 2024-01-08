6fd033c73ea3707b4750ff486bf32ff871539999
khoathuc <khoathuc141201@gmail.com>
2024-01-08 22:55:04 +0700

    add asset audit log

--- models/audit/listener.ts
+++ models/audit/listener.ts
@@ -1,6 +1,15 @@
 import prisma from "@/lib/db/prisma";
 import { getCurrentUser } from "@/lib/session";
 import { audits } from "@prisma/client";
+import { Asset } from "../asset/asset";
+
+type auditAssetData = {
+  asset_id: number;
+  object_export: { id: number | string; name: string };
+  audit_id: number;
+  user_id: number;
+  metatype: string;
+};
 
 export class Listener {
   private audit?: audits;
@@ -10,36 +19,60 @@ export class Listener {
   }
 
   async create() {
-    const user = await getCurrentUser();
+    try {
+      const user = await getCurrentUser();
 
-    if (!user) {
-      throw new Error("Invalid audit creator");
-    }
-    if (!this.audit) {
-      throw new Error("Invalid audit");
-    }
+      if (!user) {
+        throw new Error("Invalid audit creator");
+      }
+      if (!this.audit) {
+        throw new Error("Invalid audit");
+      }
 
-    await prisma.audit_logs.create({
-        data:{
-            metatype: 'create',
-            user_id: user.id,
-            audit_id: this.audit.id,
-        }
-    })
+      await prisma.audit_logs.create({
+        data: {
+          metatype: "create",
+          user_id: user.id,
+          audit_id: this.audit.id,
+        },
+      });
 
-    if(!this.audit.locations || !Array.isArray(this.audit.locations)){
-        throw new Error('Invalid location');
-    }
+      var audit_assets_data: auditAssetData[] = [];
+      const location_assets = await Asset.loader().getInLocations(
+        this.audit.locations as any,
+      );
+      location_assets.forEach((location_asset) => {
+        audit_assets_data.push({
+          asset_id: location_asset.id,
+          object_export: { id: location_asset.id, name: location_asset.name },
+          audit_id: this.audit?.id,
+          user_id: user.id,
+          metatype: "asset",
+        });
+      });
 
-    await prisma.locations.updateMany({
-        where:{
-            id:{
-                in: this.audit.locations as number[]
-            }
+      await prisma.audit_logs.createMany({
+        data: audit_assets_data
+      })
+
+      if (!this.audit.locations || !Array.isArray(this.audit.locations)) {
+        throw new Error("Invalid location");
+      }
+
+      await prisma.locations.updateMany({
+        where: {
+          id: {
+            in: this.audit.locations as number[],
+          },
         },
         data: {
-            auditing: true
-        }
-    })
+          auditing: true,
+        },
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        return new Error(error.message);
+      }
+    }
   }
 }
